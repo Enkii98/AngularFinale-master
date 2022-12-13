0053aec5007e89f1f75ee273726e7d06df54b924
FlavioCir <scatto2003@gmail.com>
2022-12-13 18:02:54 +0100

    login e register completati

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';;
 import { FormsModule } from '@angular/forms';
-import { Route, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,31 +13,6 @@ import { AddPostComponent } from './components/add-post/add-post.component';
 import { EditPostComponent } from './components/edit-post/edit-post.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-const routes: Route[] = [
-    {
-        path: "",
-        component: HomeComponent,
-        children: [
-            {
-                path: "addPost",
-                component: AddPostComponent
-            }
-        ]
-    },
-    {
-        path: "post",
-        component: PostComponent
-    },
-    {
-        path: "dettagli/:id",
-        component: DettagliPostComponent
-    },
-    {
-        path: "edit/:id",
-        component: EditPostComponent
-    }
-]
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -47,15 +21,14 @@ const routes: Route[] = [
     HomeComponent,
     DettagliPostComponent,
     AddPostComponent,
-    EditPostComponent
+    EditPostComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes),
-    NgbModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    constructor(private auth: AuthService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    logout() {
+        this.auth.logout()
+    }
 
 }
